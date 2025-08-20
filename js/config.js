66bd0d4e557b6da2dd85c4fbb07f593f07b63049
STOLENBOATS <almeida.ricardojorge@gmail.com>
2025-08-20 19:15:28 +0100

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,1 +1,6 @@
 const NAV={BUILD:'N.A.V 2.2.2 FALLBACK',STORAGE:{SESSION:'nav_session',WIN_HISTORY:'winHistory',MOTOR_HISTORY:'motorHistory'},THEME_KEY:'nav_theme'};
+window.MIEC_CONFIG = {
+  SUPABASE_URL: "https://okmifknkaidblkueiwjn.supabase.co",
+  SUPABASE_ANON_KEY: "COLA_AQUI_A_TUA_ANON_KEY",
+  STORAGE_BUCKET: "photos"
+};
