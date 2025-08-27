af83936ece3db62d7632ab8dffcbdf4a38441dc2
STOLENBOATS <almeida.ricardojorge@gmail.com>
2025-08-27 18:54:21 +0100

    config.dev.js

--- js/config.dev.js
+++ js/config.dev.js
@@ -4,5 +4,5 @@ window.MIEC_CONFIG = Object.assign({}, window.MIEC_CONFIG, {
   DEV_MODE: false,                // mude para true em DEV, ou use ?dev=1 / localStorage MIEC_DEV=1
   DEV_EMAIL: 'tester@miec.local', // <-- SUBSTITUIR pelo email DEV (opcional)
   DEV_PASSWORD: '',               // <-- SUBSTITUIR pela password DEV (opcional)
-  HIDE_LOGIN_IN_DEV: true
+  HIDE_LOGIN_IN_DEV: false
 });
